6124594bee06e439768f93d430e775efede1fd82
Jumaboy <jumaboynematov280@gmail.com>
2024-09-27 14:36:36 +0500

    model yozdim

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,35 @@
 from django.contrib import admin
-
+from .models import AboutMe, Service, Skills, Language
 # Register your models here.
+
+@admin.register(AboutMe)
+class AboutMeAdmin(admin.ModelAdmin):
+    list_display = ('id', 'full_name', 'region', 'city', 'age', 'choice')
+    list_filter = ('region', 'city', 'age', 'choice')
+    list_display_links = ('id', 'full_name', 'region', 'city',)
+    search_fields = ('full_name', 'region', 'city')
+
+
+@admin.register(Service)
+class ServiceAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'title_ru', 'title_en', 'description', 'description_ru', 'description_en')
+    list_filter = ('title', 'title_ru', 'title_en')
+    list_display_links = ('id', 'title', 'title_ru', 'title_en')
+    search_fields = ('title', 'title_ru', 'title_en',)
+
+@admin.register(Skills)
+class SkillsAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name','percentage')
+    list_filter = ('name', 'percentage')
+    list_display_links = ('id', 'name',)
+    search_fields = ('name',)
+
+
+@admin.register(Language)
+class LanguageAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'percentage')
+    list_filter = ('name', 'percentage')
+    list_display_links = ('id', 'name',)
+    search_fields = ('name',)
+
+
--- main/models.py
+++ main/models.py
@@ -2,8 +2,26 @@ from django.db import models
 
 
 class AboutMe(models.Model):
-    ...
+    full_name = models.CharField(max_length=50, verbose_name='fullname')
+    region = models.CharField(max_length=255, verbose_name='region')
+    city = models.CharField(max_length=255, verbose_name='city')
+    age = models.IntegerField(verbose_name='age')
+    profession = models.TextField(verbose_name='profession')
+    phone = models.CharField(max_length=255, verbose_name='phone')
+    email = models.EmailField(verbose_name='email')
+    address = models.TextField(verbose_name='address')
 
+    def __str__(self):
+        return self.full_name
+
+
+class  SocialMedia(models.Model):
+    name = models.CharField(max_length=150)
+    link = models.URLField()
+    image = models.ImageField(verbose_name='image')
+
+    def __str__(self):
+        return self.name
 
 class Service(models.Model):
     title = models.CharField(max_length=50)
@@ -24,3 +42,35 @@ class Skills(models.Model):
 
     def __str__(self):
         return self.name
+
+class Language(models.Model):
+    name = models.CharField(max_length=25)
+    percentage = models.IntegerField()
+
+    def __str__(self):
+         return self.name
+
+class Portfolio_Category(models.Model):
+    name = models.CharField(max_length=255)
+
+
+    def __str__(self):
+        return self.name
+
+class Project(models.Model):
+    title = models.CharField(max_length=255)
+    title_ru = models.CharField(max_length=255, blank=True, null=True)
+    title_en = models.CharField(max_length=255, blank=True, null=True)
+    description = models.TextField()
+    description_ru = models.TextField(blank=True, null=True)
+    description_en = models.TextField(blank=True, null=True)
+    category = models.ForeignKey(Portfolio_Category, on_delete=models.CASCADE)
+    image1 = models.ImageField(verbose_name='image')
+    image2 = models.ImageField(verbose_name='image', upload_to='image/', blank=True, null=True)
+    image3 = models.ImageField(verbose_name='image', upload_to='image/', blank=True, null=True)
+    image4 = models.ImageField(verbose_name='image', upload_to='image/', blank=True, null=True)
+    date_start = models.DateField(null=True, blank=True)
+    date_end = models.DateField(null=True, blank=True)
+    # status = models.
+    def __str__(self):
+        return self.title
